9f3f654c72f740d16a43e08a2006a46737103000
Greg A Morrissey <greg-morrissey@kellogg.northwestern.edu>
2013-11-10 19:34:59 -0600

    Phase II

--- app/controllers/coords_controller.rb
+++ app/controllers/coords_controller.rb
@@ -3,8 +3,22 @@ require 'json'
 
 class CoordsController < ApplicationController
   def fetch_weather
-    @latitude = 37.8267.to_s
-    @longitude = -122.423.to_s
+    @address =  if params[:address] == ""
+                    "Old Range Steakhouse, Tahoe Vista, CA"
+                else
+                    params[:address]
+                end
+    @url_safe_address = URI.encode(@address)
+
+    # Your code goes here.
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address="+@url_safe_address+"&sensor=false"
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+    @latitude = (parsed_data["results"][0]["geometry"]["location"]["lat"]).to_s
+    @longitude = (parsed_data["results"][0]["geometry"]["location"]["lng"]).to_s
+
+    #@latitude = 42.0538387.to_s
+    #@longitude = -87.67721.to_s
     your_api_key = "2bdca4828e0a4365023c66bdb8cbb230"
 
     # Your code goes here.
